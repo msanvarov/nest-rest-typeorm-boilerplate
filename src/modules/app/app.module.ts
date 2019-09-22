44e4e40f8c8ecdbbb4fc0faae04a8342c3c8f93b
msanvarov <salim.anvarov@yahoo.com>
2019-09-21 21:51:55 -0400

    :building_construction: imports to app module to reflect changes

--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -1,9 +1,59 @@
+import * as winston from 'winston';
+import * as rotateFile from 'winston-daily-rotate-file';
 import { Module } from '@nestjs/common';
+import { TypeOrmModule, TypeOrmModuleAsyncOptions } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '../config/config.module';
+import { ConfigService } from '../config/config.service';
+import { AuthModule } from '../auth/auth.module';
+import { ProfileModule } from '../profile/profile.module';
+import { WinstonModule } from '../winston/winston.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        console.log('configService -> port', configService.get('DB_PORT'));
+        return {
+          type: configService.get('DB_TYPE'),
+          host: configService.get('DB_HOST'),
+          port: configService.get('DB_PORT'),
+          username: configService.get('DB_USERNAME'),
+          password: configService.get('DB_PASSWORD'),
+          database: configService.get('DB_DATABASE'),
+          entities: [__dirname + './../**/**.entity{.ts,.js}'],
+          synchronize: configService.isEnv('dev'),
+        } as TypeOrmModuleAsyncOptions;
+      },
+    }),
+    WinstonModule.forRoot({
+      level: 'info',
+      format: winston.format.json(),
+      defaultMeta: { service: 'user-service' },
+      transports: [
+        new winston.transports.File({
+          filename: 'logs/error.log',
+          level: 'error',
+        }),
+        new winston.transports.Console({
+          format: winston.format.simple(),
+        }),
+        new rotateFile({
+          filename: 'logs/application-%DATE%.log',
+          datePattern: 'YYYY-MM-DD',
+          zippedArchive: true,
+          maxSize: '20m',
+          maxFiles: '14d',
+        }),
+      ],
+    }),
+    ConfigModule,
+    AuthModule,
+    ProfileModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
