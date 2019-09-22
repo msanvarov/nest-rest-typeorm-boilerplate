4d25d5c0942f1000992b27a87235546fde6953f3
msanvarov <salim.anvarov@yahoo.com>
2019-09-21 21:46:59 -0400

    : sparkles: created payloads for the register and login functionalities

---
+++ src/modules/auth/payload/register.payload.ts
@@ -0,0 +1,29 @@
+import { ApiModelProperty } from '@nestjs/swagger';
+import { IsAlpha, IsEmail, IsNotEmpty, MinLength } from 'class-validator';
+
+export class RegisterPayload {
+  @ApiModelProperty({
+    required: true,
+  })
+  @IsEmail()
+  email: string;
+
+  @ApiModelProperty({
+    required: true,
+  })
+  @IsAlpha()
+  username: string;
+
+  @ApiModelProperty({
+    required: true,
+  })
+  @IsAlpha()
+  name: string;
+
+  @ApiModelProperty({
+    required: true,
+  })
+  @IsNotEmpty()
+  @MinLength(8)
+  password: string;
+}
