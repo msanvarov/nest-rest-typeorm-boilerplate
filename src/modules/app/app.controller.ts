46e7da088ef9d941f32e725e68d7c8cedcc49cae
msanvarov <salim.anvarov@yahoo.com>
2019-09-21 21:51:12 -0400

    :sparkles: method for base path that requires authentication

--- src/modules/app/app.controller.ts
+++ src/modules/app/app.controller.ts
@@ -1,12 +1,16 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ApiBearerAuth } from '@nestjs/swagger';
+import { AuthGuard } from '@nestjs/passport';
 
+@ApiBearerAuth()
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @UseGuards(AuthGuard())
+  root(): string {
+    return this.appService.root();
   }
 }
