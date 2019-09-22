d1c33dec832d38be3effe783655da539e5df9b5b
msanvarov <salim.anvarov@yahoo.com>
2019-09-21 22:32:15 -0400

    :heavy_plus_sign: created auth service methods to auth controller

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,4 +1,34 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { ProfileService } from '../profile/profile.service';
+import { ConfigService } from '../config/config.service';
+import { JwtService } from '@nestjs/jwt';
+import { Profile } from '../profile/profile.entity';
+import { LoginPayload } from './payload/login.payload';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private readonly jwtService: JwtService,
+    private readonly configService: ConfigService,
+    private readonly profileService: ProfileService,
+  ) {}
+
+  async createToken(profile: Profile) {
+    return {
+      expiresIn: this.configService.get('JWT_EXPIRATION_TIME'),
+      accessToken: this.jwtService.sign({ id: profile.id }),
+      profile,
+    };
+  }
+
+  async validateUser(payload: LoginPayload) {
+    const user = await this.profileService.getByUsernameAndPass(
+      payload.username,
+      payload.password,
+    );
+    if (!user) {
+      throw new UnauthorizedException('Wrong login combination!');
+    }
+    return user;
+  }
+}
