90b8d44c1446307cebdb75055131f9130bf0beec
msanvarov <salim.anvarov@yahoo.com>
2019-09-21 22:29:27 -0400

    :heavy_plus_sign: created auth routes

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,4 +1,33 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { ApiResponse, ApiUseTags } from '@nestjs/swagger';
+import { AuthService } from './auth.service';
+import { LoginPayload } from './payload/login.payload';
+import { RegisterPayload } from './payload/register.payload';
+import { ProfileService } from '../profile/profile.service';
 
-@Controller('auth')
-export class AuthController {}
+@Controller('api/auth')
+@ApiUseTags('authentication')
+export class AuthController {
+  constructor(
+    private readonly authService: AuthService,
+    private readonly profileService: ProfileService,
+  ) {}
+
+  @Post('login')
+  @ApiResponse({ status: 201, description: 'Login Completed' })
+  @ApiResponse({ status: 400, description: 'Bad Request' })
+  @ApiResponse({ status: 401, description: 'Unauthorized' })
+  async login(@Body() payload: LoginPayload) {
+    const profile = await this.authService.validateUser(payload);
+    return await this.authService.createToken(profile);
+  }
+
+  @Post('register')
+  @ApiResponse({ status: 201, description: 'Registration Completed' })
+  @ApiResponse({ status: 400, description: 'Bad Request' })
+  @ApiResponse({ status: 401, description: 'Unauthorized' })
+  async register(@Body() payload: RegisterPayload) {
+    const profile = await this.profileService.create(payload);
+    return await this.authService.createToken(profile);
+  }
+}
