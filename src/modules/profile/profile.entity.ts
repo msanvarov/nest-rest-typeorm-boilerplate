12998c4acda9def76cd7ac3df22b46df50bea135
msanvarov <salim.anvarov@yahoo.com>
2019-09-21 21:49:09 -0400

    :heavy_plus_sign: profile entity and password transformer

---
+++ src/modules/profile/profile.entity.ts
@@ -0,0 +1,28 @@
+import { Exclude } from 'class-transformer';
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import { PasswordTransformer } from './password.transformer';
+
+@Entity({
+  name: 'profiles',
+})
+export class Profile {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  username: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  email: string;
+
+  @Column({
+    name: 'password',
+    length: 255,
+    transformer: new PasswordTransformer(),
+  })
+  @Exclude()
+  password: string;
+}
