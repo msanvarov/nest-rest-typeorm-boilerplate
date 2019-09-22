d2206f1a1e5d66018584727ed8d3980c01f5bfdc
msanvarov <salim.anvarov@yahoo.com>
2019-09-21 23:17:39 -0400

    :art: added more routes for testing

--- src/modules/app/app.controller.ts
+++ src/modules/app/app.controller.ts
@@ -1,16 +1,35 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import { Controller, Delete, Get, Param, Req, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
-import { ApiBearerAuth } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiResponse } from '@nestjs/swagger';
 import { AuthGuard } from '@nestjs/passport';
+import { ProfileService } from '../profile/profile.service';
 
 @ApiBearerAuth()
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly profileService: ProfileService,
+  ) {}
 
   @Get()
   @UseGuards(AuthGuard())
   root(): string {
     return this.appService.root();
+  }
+
+  @Get('/api/profile')
+  @UseGuards(AuthGuard())
+  @ApiResponse({ status: 200, description: 'Request Received' })
+  @ApiResponse({ status: 400, description: 'Request Failed' })
+  getProfile(@Req() req) {
+    return req.user;
+  }
+
+  @Delete('/api/profile/:username')
+  @ApiResponse({ status: 200, description: 'Request Received' })
+  @ApiResponse({ status: 400, description: 'Request Failed' })
+  async delete(@Param('username') username: string) {
+    return await this.profileService.delete(username);
   }
 }
