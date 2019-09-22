93d5263c879563ad578061aeaed9e5dc82be67ff
msanvarov <salim.anvarov@yahoo.com>
2019-09-21 23:18:53 -0400

    :heavy_plus_sign: implemented method to create user

--- src/modules/profile/profile.service.ts
+++ src/modules/profile/profile.service.ts
@@ -1,5 +1,9 @@
 import * as crypto from 'crypto';
-import { Injectable, NotAcceptableException } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotAcceptableException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 
@@ -18,21 +22,21 @@ export class ProfileService {
   }
 
   async getByUsername(username: string) {
-    return await this.profileRepository.findOne({
-      where: {
-        username,
-      },
-    });
+    return await this.profileRepository
+      .createQueryBuilder('profiles')
+      .where('profiles.username = :username')
+      .setParameter('username', username)
+      .getOne();
   }
 
   async getByUsernameAndPass(username: string, password: string) {
     const hashedPass = crypto.createHmac('sha256', password).digest('hex');
-    return await this.profileRepository.findOne({
-      where: {
-        username,
-        password: hashedPass,
-      },
-    });
+    return await this.profileRepository
+      .createQueryBuilder('profiles')
+      .where('profiles.username = :username and profiles.password = :password')
+      .setParameter('username', username)
+      .setParameter('password', hashedPass)
+      .getOne();
   }
 
   async create(payload: RegisterPayload) {
@@ -47,5 +51,16 @@ export class ProfileService {
     return await this.profileRepository.save(
       this.profileRepository.create(payload),
     );
+  }
+
+  async delete(username: string) {
+    const deleted = await this.profileRepository.delete({ username });
+    if (deleted.affected === 1) {
+      return { message: `Deleted ${username} from records` };
+    } else {
+      throw new BadRequestException(
+        `Failed to delete a profile by the name of ${username}.`,
+      );
+    }
   }
 }
