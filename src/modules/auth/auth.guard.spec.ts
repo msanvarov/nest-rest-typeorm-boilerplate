315ec592437bb628a8535574bff85f0f295de7b3
msanvarov <salim.anvarov@yahoo.com>
2019-09-23 13:01:00 -0400

    :fire: removed useless auth guard

--- src/modules/auth/auth.guard.spec.ts
+++
@@ -1,7 +0,0 @@
-import { AuthenticationGuard } from './auth.guard';
-
-describe('AuthGuard', () => {
-  it('should be defined', () => {
-    expect(new AuthenticationGuard()).toBeDefined();
-  });
-});
