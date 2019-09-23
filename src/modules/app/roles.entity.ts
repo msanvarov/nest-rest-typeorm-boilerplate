e5c533b2e7648dafeebe5abe74586af29cb3ed6f
msanvarov <salim.anvarov@yahoo.com>
2019-09-23 13:02:19 -0400

    :sparkles: created roles entity and modified profile to use it

---
+++ src/modules/app/roles.entity.ts
@@ -0,0 +1,20 @@
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne } from 'typeorm';
+import { AppRoles } from './app.roles';
+import { Profile } from '../profile/profile.entity';
+
+@Entity()
+export class Roles {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  //   beware this deafult app role will permit every profile to delete other profiles
+  @Column({
+    type: 'enum',
+    enum: AppRoles,
+    default: AppRoles.ADMIN_DELETE_PROFILES,
+  })
+  role: AppRoles;
+
+  @ManyToOne(type => Profile, profile => profile.roles, { onDelete: 'CASCADE' })
+  profile: Profile;
+}
--- src/modules/profile/profile.entity.ts
+++ src/modules/profile/profile.entity.ts
@@ -1,6 +1,7 @@
 import { Exclude } from 'class-transformer';
-import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import { Entity, Column, PrimaryGeneratedColumn, OneToMany } from 'typeorm';
 import { PasswordTransformer } from './password.transformer';
+import { Roles } from '../app/roles.entity';
 
 @Entity({
   name: 'profiles',
@@ -9,7 +10,7 @@ export class Profile {
   @PrimaryGeneratedColumn()
   id: number;
 
-  @Column()
+  @Column({ unique: true })
   username: string;
 
   @Column()
@@ -17,6 +18,9 @@ export class Profile {
 
   @Column()
   email: string;
+
+  @OneToMany(type => Roles, role => role.profile)
+  roles: Roles[];
 
   @Column({
     name: 'password',
