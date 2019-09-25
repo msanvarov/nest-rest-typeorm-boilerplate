ed806efad933ad50a5514e8e19cf62ca2122457b
msanvarov <salim.anvarov@yahoo.com>
2019-09-25 14:39:32 -0400

    :ok_hand: reflecting changes to code by importing passport module and controller modules

--- src/modules/profile/profile.module.ts
+++ src/modules/profile/profile.module.ts
@@ -3,10 +3,16 @@ import { ProfileService } from './profile.service';
 import { Profile } from './profile.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Roles } from '../app/roles.entity';
+import { ProfileController } from './profile.controller';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Profile, Roles])],
+  imports: [
+    TypeOrmModule.forFeature([Profile, Roles]),
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+  ],
   providers: [ProfileService],
   exports: [ProfileService],
+  controllers: [ProfileController],
 })
 export class ProfileModule {}
