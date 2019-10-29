547da8cfd937291a7deec55204c2eed57b084f16
msanvarov <salim.anvarov@yahoo.com>
2019-10-29 13:02:47 -0400

    :art: code style changes

--- src/modules/winston/winston.constants.ts
+++ src/modules/winston/winston.constants.ts
@@ -1,11 +1,10 @@
-  
 /**
  * Token for the type of configuration to be used when declaring in app module
  * @type{string}
  */
-export const WINSTON_MODULE_OPTIONS = "WinstonModuleOptions";
+export const WINSTON_MODULE_OPTIONS = 'WinstonModuleOptions';
 /**
  * An identifier for when injecting the winston provider
  * @type{string}
  */
-export const WINSTON_MODULE_PROVIDER = "winston";
+export const WINSTON_MODULE_PROVIDER = 'winston';
