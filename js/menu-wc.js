774b65ba5ecc7961611cba961f9145b6909dfb0a
msanvarov <msalanvarov@gmail.com>
2022-09-29 21:48:06 +0000

    Deploying to gh-pages from @ msanvarov/nest-rest-typeorm-boilerplate@e7693e4d1661a68dc726c14b3664ca91b0fd6199 🚀

--- js/menu-wc.js
+++ js/menu-wc.js
@@ -33,6 +33,11 @@ customElements.define('compodoc-menu', class extends HTMLElement {
                                 <span class="icon ion-ios-paper"></span>README
                             </a>
                         </li>
+                        <li class="link">
+                            <a href="license.html"  data-type="chapter-link">
+                                <span class="icon ion-ios-paper"></span>LICENSE
+                            </a>
+                        </li>
                                 <li class="link">
                                     <a href="dependencies.html" data-type="chapter-link">
                                         <span class="icon ion-ios-list"></span>Dependencies
